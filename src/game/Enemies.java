ba34b6aaa77fc6b1abb4cd3592865bc4f4216995
Dav1200 <davinder.singh@city.ac.uk>
2022-06-05 13:19:46 +0100

    Movement

--- src/game/Enemies.java
+++ src/game/Enemies.java
@@ -13,9 +13,7 @@ public class Enemies extends Walker implements StepListener {
     private GameWorld world;
     private Vec2 PlayerPos;
     private String facing;
-    private  Boolean move;
-
-
+    private Boolean move;
 
 
     public Enemies(GameWorld world) {
@@ -34,48 +32,44 @@ public class Enemies extends Walker implements StepListener {
         PlayerPos = world.getStudent().getPosition();
 
 
-        if(PlayerPos.x < this.getPosition().x && this.getPosition().y - PlayerPos.y  > -1  && this.getPosition().y - PlayerPos.y  < 1 ){
-            move =false;
-            setLinearVelocity(new Vec2(-6 ,0));
-        }
-
-        else if(PlayerPos.x > this.getPosition().x && this.getPosition().y - PlayerPos.y  > -1  && this.getPosition().y - PlayerPos.y  < 1 )
-        { move =false;
-            setLinearVelocity(new Vec2(6 ,0));}
-        else if(PlayerPos.y > this.getPosition().y && this.getPosition().x - PlayerPos.x  > -1  && this.getPosition().x - PlayerPos.x  < 1 )
-        { move =false;
-            setLinearVelocity(new Vec2(0 ,6));}
-
-        else if(PlayerPos.y < this.getPosition().y && this.getPosition().x - PlayerPos.x  > -1  && this.getPosition().x - PlayerPos.x  < 1 )
-        { move =false;
-            setLinearVelocity(new Vec2(0 ,-6));}
-
-        else{
+        if (PlayerPos.x < this.getPosition().x && this.getPosition().y - PlayerPos.y > -1 && this.getPosition().y - PlayerPos.y < 1) {
+            move = false;
+            setLinearVelocity(new Vec2(-6, 0));
+        } else if (PlayerPos.x > this.getPosition().x && this.getPosition().y - PlayerPos.y > -1 && this.getPosition().y - PlayerPos.y < 1) {
+            move = false;
+            setLinearVelocity(new Vec2(6, 0));
+        } else if (PlayerPos.y > this.getPosition().y && this.getPosition().x - PlayerPos.x > -1 && this.getPosition().x - PlayerPos.x < 1) {
+            move = false;
+            setLinearVelocity(new Vec2(0, 6));
+        } else if (PlayerPos.y < this.getPosition().y && this.getPosition().x - PlayerPos.x > -1 && this.getPosition().x - PlayerPos.x < 1) {
+            move = false;
+            setLinearVelocity(new Vec2(0, -6));
+        } else {
             move = true;
-            setLinearVelocity(new Vec2(0,0));
+            setLinearVelocity(new Vec2(0, 0));
         }
 
 
-        if(move) {
+        if (move) {
             if (PlayerPos.x < this.getPosition().x && PlayerPos.y > this.getPosition().y) {
 
 
-                setLinearVelocity(new Vec2(-6,6));
+                setLinearVelocity(new Vec2(-6, 6));
             }
 
 
             if (PlayerPos.x > this.getPosition().x && PlayerPos.y > this.getPosition().y) {
-                setLinearVelocity(new Vec2(6,6));
+                setLinearVelocity(new Vec2(6, 6));
 
             }
 
             if (PlayerPos.x < this.getPosition().x && PlayerPos.y < this.getPosition().y) {
-                 setLinearVelocity(new Vec2(-6,-6));
+                setLinearVelocity(new Vec2(-6, -6));
 
             }
 
             if (PlayerPos.x > this.getPosition().x && PlayerPos.y < this.getPosition().y) {
-                setLinearVelocity(new Vec2(6,-6));
+                setLinearVelocity(new Vec2(6, -6));
 
             }
 
